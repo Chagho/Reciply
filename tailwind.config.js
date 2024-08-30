62f760687ff13b65aa982e742b8e6c03918fa972
Eren Çağrı Demirci <erencdem@gmail.com>
2024-08-31 01:25:41 +0300

    first commit/landing page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        pixelify: 'Pixelify sans'
+      }
+    },
+  },
+  plugins: [],
+}
+
