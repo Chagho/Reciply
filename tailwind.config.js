7a4680820fd9a53be52885de0f723ccd124bef7f
Eren Çağrı Demirci <erencdem@gmail.com>
2024-08-31 22:33:09 +0300

    second commit/results page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       fontFamily: {
         pixelify: 'Pixelify sans'
+      },
+      backgroundImage: {
+        foodbg: "url('/images/food-bg.png')"
       }
     },
   },
